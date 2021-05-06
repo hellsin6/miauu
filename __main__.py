f0a654ebae81c616fe2b19b7942d7fbbe0036e15
Hell <hellsing.otaku@gmail.com>
2021-05-06 17:31:55 -0400

    llego la parte del luft miau miau

--- __main__.py
+++ __main__.py
@@ -1,25 +1,33 @@
 from g_python.hpacket import HPacket
-import socket, binascii
+import socket, binascii, requests
+
+luftbb = requests.get("https://api.harble.net/messages/latest.json").json()
+latest_version = luftbb["Revision"]
+
+
+def find_id(msg, type):
+	for x in luftbb[type]:
+		if x["Name"] == msg:
+			return x["Id"]
 
 
 def main():
-    client_socket = socket.socket()
-    client_socket.connect(("game-es.habbo.com", 30000))
+	client_socket = socket.socket()
+	client_socket.connect(("game-es.habbo.com", 30000))
 
-    # {ClientHello}{s:"WIN63-202104221206-601649277"}{s:"FLASH3"}{i:1}{i:0}
-    start = HPacket(4000, "WIN63-202104221206-601649277", "FLASH3", 1, 0)
-    diffie = HPacket(2329)
+	start = HPacket(find_id("ClientHello", "Outgoing"), latest_version, "FLASH3", 1, 0)
+	diffie = HPacket(find_id("InitDiffieHandshake", "Outgoing"))
 
-    client_socket.send(bytes(start))
-    client_socket.send(bytes(diffie))
+	client_socket.send(bytes(start))
+	client_socket.send(bytes(diffie))
 
-    data = client_socket.recv(1024)
-    if len(data) > 0:
-        print(binascii.b2a_hex(data))
-        client_socket.send(bytes(HPacket(2346)))
+	data = client_socket.recv(1024)
+	if len(data) > 0:
+		print(binascii.b2a_hex(data))
+		client_socket.send(bytes(HPacket(2346)))
 
-    client_socket.close()
+	client_socket.close()
 
 
 if __name__ == '__main__':
-    main()
+	main()
